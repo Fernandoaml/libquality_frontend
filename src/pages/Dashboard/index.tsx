64f18ada79c2567670b7e5f647e616579c9c5440
Fernando A M Leite <fernandomottaptc@gmail.com>
2020-09-06 21:09:24 -0300

    added Prettier to project

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
 
-const Dashboard: React.FC = () => <h1>Dashboard</h1>;
+const Dashboard: React.FC = () => {
+  const a = 1;
+  return (
+    <>
+      <h1>LibQuality</h1>
+      <h3>This is a simple tool to compare quality of diferent ioen source libraries availavle in GitHub</h3>
+    </>
+  );
+};
 
 export default Dashboard;
